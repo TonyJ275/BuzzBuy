1a94b33ff2e2960df19cc82294b7f85829d4dad2
TonyJ275 <tonyjain177@gmail.com>
2023-07-01 00:59:11 +0530

    initial backend setup

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,18 +1,30 @@
-import { Row, Col } from "react-bootstrap"
-import products from "../products"
-import Product from "../components/Product"
+import { Row, Col } from "react-bootstrap";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import Product from '../components/Product';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
-    <h1> Latest Products </h1>
-    <Row>
+      <h1> Latest Products </h1>
+      <Row>
         {products.map((product) => (
-            <Col id={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-            </Col>
-        )) }
-    </Row>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </>
   )
 }
